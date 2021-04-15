115f9c660474b049233047e15167b4bc301a9197
Misha <bobovmikhail@mail.ru>
2021-04-15 15:44:45 +0300

    '11th'

--- homework1/sample_project/tests/test_calculator.py
+++ homework1/sample_project/tests/test_calculator.py
@@ -18,4 +18,4 @@ def test_negative_case2():
 
 def test_negative_case3():
     """Testing that non-int input() will not crash the program"""
-    assert not calculator.calc.check_power_of_2('some string')
+    assert not calculator.calc.check_power_of_2("some string")
