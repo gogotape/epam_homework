7f236f4ad7be6e4ac788bfd0e3e7ebb0e002f591
Misha <bobovmikhail@mail.ru>
2021-05-01 17:51:16 +0300

    'add_task2_hw2'

--- homework3/task02/task02.py
+++ homework3/task02/task02.py
@@ -23,11 +23,16 @@ import hashlib
 import random
 import struct
 import time
+from multiprocessing import Pool
 
 
 def slow_calculate(value):
     """Some weird voodoo magic calculations"""
     time.sleep(random.randint(1, 3))
     data = hashlib.md5(str(value).encode()).digest()
-    times = datetime.now() - start
     return sum(struct.unpack("<" + "B" * len(data), data))
+
+
+def do():
+    with Pool() as p:
+        return sum(p.map(slow_calculate, range(500)))
---
+++ homework3/test_task02/test_task02.py
@@ -0,0 +1,7 @@
+# result = 1024259
+from task02.task02 import *
+
+"""
+def test_example():
+    assert do() == 1024259
+"""
